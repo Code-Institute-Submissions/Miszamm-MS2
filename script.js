b035500f37d46da914efefff0edab70657e2ee81
Michal Misza <miszamm@yahoo.ie>
2020-10-21 14:44:39 +0000

    creating a function to generate html in script.js, function loop thru and generate result for each item

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const container = document.querySelector('.container');
 let searchQuery = '';
 const APP_ID = '72dfbd62';
 const APP_key = 'd0ec890c12b422a408b0562a3572d6d4';
-const baseURL = `https://api.edamam.com/search?q=fish&app_id=${APP_ID}&app_key=${APP_key}`;
+const baseURL = `https://api.edamam.com/search?q=cake&app_id=${APP_ID}&app_key=${APP_key}`;
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -13,7 +13,32 @@ searchForm.addEventListener('submit', (e) => {
 });
 
 async function fetchAPI(){
-const baseURL = `https://api.edamam.com/search?q=fish&app_id=${APP_ID}&app_key=${APP_key}`;
+const baseURL = `https://api.edamam.com/search?q=cake&app_id=${APP_ID}&app_key=${APP_key}&to=20`;
 const response = await fetch(baseURL);
-console.log(response);
+const data = await response.json();
+generateHTML(data.hits);
+console.log(data);
 };
+function generateHTML(results){
+    let generatedHTML = '';
+    results.map(result => {
+        generatedHTML +=
+        ` <div class="item">
+            <img src="img/ms2-photo.jpg" alt="">
+             <div class="flex-container">
+              <h2 class="recipe-name">This is...</h2>
+             <a href="#">See Recipe</a>
+          </div>
+         <p class="item-info">Estimated Cal: </p>
+         <p class="item-info">Makronutrients:
+          <ul>
+           <li>Carbohydrates</li>
+           <li>Protein</li>
+           <li>Fats</li>
+          </ul>
+         </p>
+        </div>
+        `
+    })
+   searchOutcomeDiv.innerHTML = generatedHTML; 
+}
