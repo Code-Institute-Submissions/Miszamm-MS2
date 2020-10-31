749d42d01b68b208364e41ad7b5c187961c06c6c
Michal Misza <miszamm@yahoo.ie>
2020-10-31 14:37:18 +0000

    modal added to html and variables and finctions to open and close modal added

--- script.js
+++ script.js
@@ -5,16 +5,37 @@ let searchQuery = '';
 const APP_ID = '72dfbd62';
 const APP_key = 'd0ec890c12b422a408b0562a3572d6d4';
 const baseURL = `https://api.edamam.com/search?q=cake&app_id=${APP_ID}&app_key=${APP_key}`;
+
 var modal = document.getElementById('macroModal');
 var modalBtn = document.getElementById('modalBtn');
 var closeBtn = document.getElementsByClassName('closeBtn')[0];
+//listen for  open click
+modalBtn.addEventListener('click', openModal );
+//listen for close click
+modalBtn.addEventListener('click', closeModal );
+//listen for aoutside click
+window.addEventListener('click', outsideClick);
 
+//function to open modal
+function openModal(){
+    modal.style.display = 'block';
+}
+//function to close modal
+function closeModal(){
+    modal.style.display = 'none';
+}
+//function to close modal if click outside the block
+function outsideClick(e){
+    if(e.target == modal){
+    modal.style.display = 'none';
+    }
+}
 
 searchForm.addEventListener('submit', (e) => {
-    let messages = []
-    if(searchForm.value === ''|| searchForm.value == null) {
-        messages.push('No data found')
-    }
+  //  let messages = []
+   // if(searchForm.value === ''|| searchForm.value == null) {
+    //    messages.push('No data found')
+   // }
     e.preventDefault();
     searchQuery = e.target.querySelector('input').value;
     fetchAPI();
