becfa501e1b7dbd1e8e69da82b3bffe0f4b5abdd
Michal Misza <miszamm@yahoo.ie>
2020-11-03 13:48:12 +0000

    added conditional,modal removed

--- script.js
+++ script.js
@@ -6,36 +6,9 @@ const APP_ID = '72dfbd62';
 const APP_key = 'd0ec890c12b422a408b0562a3572d6d4';
 const baseURL = `https://api.edamam.com/search?q=cake&app_id=${APP_ID}&app_key=${APP_key}`;
 
-var modal = document.getElementById('macroModal');
-var modalBtn = document.getElementById('modalBtn');
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
-//listen for  open click
-modalBtn.addEventListener('click', openModal );
-//listen for close click
-modalBtn.addEventListener('click', closeModal );
-//listen for aoutside click
-window.addEventListener('click', outsideClick);
 
-//function to open modal
-function openModal(){
-    modal.style.display = 'block';
-}
-//function to close modal
-function closeModal(){
-    modal.style.display = 'none';
-}
-//function to close modal if click outside the block
-function outsideClick(e){
-    if(e.target == modal){
-    modal.style.display = 'none';
-    }
-}
 
 searchForm.addEventListener('submit', (e) => {
-  //  let messages = []
-   // if(searchForm.value === ''|| searchForm.value == null) {
-    //    messages.push('No data found')
-   // }
     e.preventDefault();
     searchQuery = e.target.querySelector('input').value;
     fetchAPI();
@@ -45,10 +18,13 @@ async function fetchAPI(){
 const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&to=40`;
 const response = await fetch(baseURL);
 const data = await response.json();
+if(data.hits.length === 0){
+    alert('No data found')
+}else{
 generateHTML(data.hits);
 console.log(data);
+}
 };
-
 
 function generateHTML(results){
     container.classList.remove('intro')
@@ -65,13 +41,6 @@ function generateHTML(results){
           <p class="item-info">Source: ${result.recipe.source}</p>
           <p class="item-info">Estimated Cal: ${result.recipe.calories.toFixed(0)}</p>
           <p class="item-info">Macronutrients:
-           <button id="modalBtn" class="button">View all</button>
-                <div id="macroModal" class="modal">
-                    <div class="modal-content">
-                        <span class="closeBtn">&times</span>
-                        <p>detailed list of macros</p>
-                    </div>
-                </div>
            <ul>
             <li class="item-info-data">Carbohydrates: ${result.recipe.totalNutrients.CHOCDF.quantity.toFixed(1)}</li>
             <li class="item-info-data">Proteins: ${result.recipe.totalNutrients.PROCNT.quantity.toFixed(1)}</li>
@@ -83,3 +52,5 @@ function generateHTML(results){
     })
    searchOutcomeDiv.innerHTML = generatedHTML; 
 }
+
+
